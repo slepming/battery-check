b3e7bd0c5418f6bf1faf193176ffd409bb978e98
slepming <siskaura06@gmail.com>
2026-03-03 09:22:52 +0300

    added loop, thread sleep for 1 sec and added cache file path to battery

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{
-    fs::{ReadDir, read_dir},
+    fs::{read, read_dir},
+    path::PathBuf,
+    thread,
     time::Duration,
 };
 
@@ -9,28 +11,46 @@ use hyprland::{
 };
 
 fn main() -> Result<(), Error> {
-    let dir: ReadDir = read_dir("/sys/class/power_supply").expect("directory error");
-    dir.for_each(|f| {
-        let file = f.unwrap();
-        if file.path().is_dir() {
-            let files = read_dir(file.path()).unwrap();
-            files.for_each(|f| {
-                let file = f.unwrap();
-                if file.path().is_file() {
-                    if file.path().file_name().unwrap() == "capacity" {
-                        let percentage: i32 = String::from_utf8(read(file.path()).unwrap())
-                            .unwrap()
-                            .parse()
-                            .unwrap();
-                        if percentage <= 15 {
-                            battery_low(file.path().to_str().unwrap().to_string());
-                        }
-                    }
-                }
-            });
+    let mut c_path: Option<PathBuf> = None;
+    loop {
+        thread::sleep(Duration::from_secs(1));
+        if let Some(path) = &c_path {
+            let percentage_raw = String::from_utf8(read(path).unwrap()).unwrap();
+            let percentage = get_percentage(percentage_raw);
+            if percentage <= 20 {
+                battery_low(format!("Capacity {}", percentage));
+            }
+            continue;
         }
-    });
-    Ok(())
+        let dir = read_dir("/sys/class/power_supply").expect("directory error");
+        dir.for_each(|f| {
+            let file = f.expect("DirEntry error");
+            if file.path().is_dir() {
+                let files = read_dir(file.path()).unwrap();
+                files.for_each(|f| {
+                    let file = f.unwrap();
+                    if file.path().is_file() && file.path().file_name().unwrap() == "capacity" {
+                        let percentage_raw = String::from_utf8(read(file.path()).unwrap()).unwrap();
+                        let percentage = get_percentage(percentage_raw);
+                        if percentage <= 20 {
+                            battery_low(format!("Capacity {}", percentage));
+                        }
+                        c_path = Some(file.path());
+                    }
+                });
+            }
+        });
+    }
+}
+
+fn get_percentage(path: String) -> i32 {
+    match path.split_once('\n') {
+        Some((key, _value)) => key.parse::<i32>().unwrap(),
+        None => {
+            dbg!("you haven't battery");
+            0
+        }
+    }
 }
 
 fn battery_low(battery: String) {
